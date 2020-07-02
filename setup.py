8d275a04750f4bc40e0d0f7d0aebcecb36a11baf
yardenas <yardas@student.ethz.ch>
2020-07-02 22:20:59 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import setup
+
+assert sys.version_info.major == 3 and sys.version_info.minor >= 6
+
+setup(
+    name='mbpo',
+    packages=['mbpo'],
+    install_requires=[
+        'gym~=0.15.3',
+        'mujoco_py==2.0.2.7',
+        'numpy~=1.17.4',
+        'tensorflow>=2.1.0',
+        'tensorflow-probability',
+        'moviepy>=1.0.0'
+    ]
+)
